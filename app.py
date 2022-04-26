245f2fb95a0e904bb0b5c4bbbf6b34c2554025c1
sharmalondon16 <sharmalondon16@gmail.com>
2022-04-26 09:54:06 +0100

    working version 1

--- app.py
+++ app.py
@@ -8,20 +8,37 @@ class Shape(ABC):
         NotImplementedError
 
 
-class Square(Shape):
 
-    sides = 4
 
-    def __init__(self,length):
+
+class Polygon(Shape):
+
+
+    def __init__(self,sides,length):
         self.side_length = length
+        self.sides = sides
 
     def perimeter(self):
-        return self.side_length*4
+        return self.side_length*self.sides
+
+
+class Square(Polygon):
+
+    sides = 4
+    def __init__(self,length):
+        super().__init__(Square.sides, length)
 
     def area(self):
-        return self.side_length**2
+        return Square.sides*self.side_length
+
+    def __str__(self):
+        return f'I am a {Square.sides} sided polygon with area {Square.sides*self.side_length}'
+
+
 
 
 if __name__ == '__main__':
     s = Square(5)
+    print(s)
+
 
